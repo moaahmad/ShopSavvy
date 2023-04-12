04486fea114d913fb3fb7e4b49ad172c9a2ece8f
Mo Ahmad <moaahmad@gmail.com>
2023-04-12 16:48:29 +0100

    Initial Commit

---
+++ ShopSavvy/ShopSavvyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopSavvyApp.swift
+//  ShopSavvy
+//
+//  Created by Mo Ahmad on 12/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShopSavvyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
